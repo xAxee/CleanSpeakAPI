7275d1c4e442ec1c1033a4adbd2e2cc4d4064fb2
xAxee <50153178+xAxee@users.noreply.github.com>
2025-07-23 19:03:46 +0200

    Fix xUnit1012 warning in TextNormalizerTests

--- CleanSpeakAPI.Tests/TextNormalizerTests.cs
+++ CleanSpeakAPI.Tests/TextNormalizerTests.cs
@@ -9,7 +9,7 @@ public class TextNormalizerTests
     [InlineData("Hello World!", "hello world")]
     [InlineData("1234 ABC!", "1234 abc")]
     [InlineData("", "")]
-    [InlineData(null, "")]
+    [InlineData("     ", " ")]
     public void Normalize_RemovesSpecialCharactersAndLowercases(string input, string expected)
     {
         var result = TextNormalizer.Normalize(input);
